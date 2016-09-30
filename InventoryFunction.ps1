dbc0b4ac3f57805480fd152893af1453a5ae8e6e
klevalley2 <keith.1985.kl@gmail.com>
2016-09-30 13:32:49 -0400

    Initial Upload

---
+++ InventoryFunction.ps1
@@ -0,0 +1,63 @@
+﻿<#
+.Synopsis
+   Allows you to retrieve win32_bios information from multiple Computers
+.DESCRIPTION
+   Commandlet uses an Active Directory query to return a Computer names as plain strings.
+   It then will use the invoke-command to retrieve the win32_bios information on each object.
+.EXAMPLE
+   Display to the screen all the Serial Numbers for each computer starting with LTCKEITHLAB
+   Get-ADInventory -searchParameter LTCKEITHLAB*
+
+.EXAMPLE
+   Save as a CSV file all the Serial Numbers for each computer starting with LTCKEITHLAB
+   Get-ADInventory -searchParameter LTCKEITHLAB* -outFileName test.csv
+
+.EXAMPLE
+   Display to the screen for each computer starting with LTCKEITHLAB all the Properties of win32_bios
+   Get-ADInventory -searchParameter LTCKEITHLAB* -Property *
+#>
+
+function Get-ADInventory
+{
+    param (
+        [Parameter(Mandatory=$True)]
+        [String]$searchParameter,
+        [Parameter()]
+        [String]$outFileName,
+        [Parameter()]
+        [String[]]$Property = ("PSComputerName", "SerialNumber")
+    )
+
+    $PClist = (Get-ADComputer -Filter "Name -like '$searchParameter'" | Select-Object -ExpandProperty Name)
+    Write-Verbose "Following PC's were found on Active Directory `n$PClist"
+
+    $PCInfo = Invoke-Command -ComputerName $PClist {Get-WmiObject -Class win32_bios | Select-Object -Property $Property}
+
+    if ($outFileName -eq ""){
+        $PCInfo | Select-Object -Property $Property | Sort-Object -Property PSComputerName
+        }
+    else{
+        $PCInfo | Select-Object -Property $Property | Sort-Object -Property PSComputerName |
+        Export-Csv -Path ".\$outFileName"
+    }
+} # End Function Get-ADInventory
+
+function Get-ADQuery
+{
+    param (
+        [Parameter(Mandatory=$True)]
+        [String]$searchParameter,
+        [Parameter()]
+        [ValidateSet("String","Object")]
+        [String]$Type = "Object",
+        [Parameter()]
+        [String[]]$Property = "*"
+    )
+    
+    if ($Type -eq "String"){
+        Get-ADComputer -Filter "Name -like '$searchParameter'" | Select-Object -ExpandProperty Name
+    }
+    else{
+        Get-ADComputer -Filter "Name -like '$searchParameter'" | Select-Object -Property $Property
+    }
+} # End Function Get-ADQuery
